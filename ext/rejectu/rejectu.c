b2a92a4b0f15a7475110628ba21d722c6e80af67
Scott Francis <scott.francis@shopify.com>
2014-06-23 19:07:12 +0000

    Add valid? method

--- ext/rejectu/rejectu.c
+++ ext/rejectu/rejectu.c
@@ -1,7 +1,43 @@
 #include <ruby.h>
 
+static VALUE mRejectu = Qnil;
+
+static VALUE
+is_valid(VALUE self, VALUE str)
+{
+  unsigned char *p, *end;
+  long len, remain;
+
+  Check_Type(str, T_STRING);
+  len = RSTRING_LEN(str);
+  p = RSTRING_PTR(str);
+  end = RSTRING_END(str);
+
+  while (p < end) {
+    if (end - p < 8)
+      break;
+
+    if (((*((uint64_t *) p) & 0xf0f0f0f0f0f0f0f0) & 0x8080808080808080) != 0)
+      return Qfalse;
+
+    p += 8;
+  }
+
+  remain = end - p;
+  while (remain) {
+    if (((*((unsigned char *) p) & 0xf0) & 0x80) != 0)
+      return Qfalse;
+    p++;
+    remain = end - p;
+  }
+
+  return Qtrue;
+}
+
 void
 Init_rejectu()
 {
+  mRejectu = rb_define_module("Rejectu");
 
+  rb_define_singleton_method(mRejectu, "valid?", is_valid, 1);
 }
--- test/test_rejectu.rb
+++ test/test_rejectu.rb
@@ -2,5 +2,26 @@ require 'test/unit'
 require 'rejectu/rejectu'
 
 class TestRejectu < Test::Unit::TestCase
+  def test_valid_only_accepts_string
+    assert_raises TypeError do
+      Rejectu::valid?(Object.new)
+    end
+  end
+
+  def test_valid_returns_boolean
+    assert Rejectu::valid? "test string"
+  end
+
+  def test_invalid_string
+    refute Rejectu::valid? "\xf2\xa4\xb7\xa4 test string"
+  end
+
+  def test_invalid_string2
+    refute Rejectu::valid? "teststri\xf2\xa4\xb7\xa4"
+  end
+
+  def test_invalid_string3
+    refute Rejectu::valid? "teststri12\xf2\xa4\xb7\xa4"
+  end
 
 end
